76f381d1f6aed4c67df9b02372f871fe79953b2b
T Mark <tmk1.tran@gmail.com>
2023-08-08 22:28:02 -0500

    started part 3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,15 +3,40 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 let basket = [];
 
-function addItem() {
-    basket.push('Kale');
+const maxItems = 5; //set a global constant of 5 items allowed in the basket
+
+let isFull = false;
+
+function addItem( item ) {
+    basket.push(item); //adds item
+    return true; // after successfully adding item
 }
 
+console.log( basket );  // should be empty in console
 
 
+let item1 = addItem( 'Kale' );  //defined a 
+console.log( item1 );
+console.log( basket ); 
+
+let item2 = addItem( 'Ice Cream' );
+console.log( item2 );
+console.log( basket );
 
 
+function listItems() { //start listItems
+    for ( let item of basket ) {  // loop to run through basket 
+        console.log( item );  // then log the items in it
+    }
+}  // end listItems
 
+
+listItems();  // running function listItems to test in console
+
+addItem( 'Bread' );  // added an item for test
+console.log( basket );  
+
+listItems();  // test after adding an item
 
 
 
